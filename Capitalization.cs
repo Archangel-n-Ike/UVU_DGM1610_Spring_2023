76a8f757ba9b4d55602ee9c81dbc5b059c5d2e2c
Michael Bailey <migueldbailey@gmail.com>
2023-01-18 21:06:46 -0700

    Does capitalization matter?
    I wanted to see if capitalizing your code changed anything, as far as I can tell it doesn't but my vscode debug isn't quite working for c# yet so I don't know for sure

---
+++ Capitalization.cs
@@ -0,0 +1,29 @@
+using System;
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Hello World");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Does Capitalization Matter?");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            console.writeline("Doesn't seem like it");
+        }
+    }
+}
