a1f49f2c2b80e8d9faa7f176547c2757817c22fb
Michael Bailey <migueldbailey@gmail.com>
2023-01-25 18:29:46 -0700

    Fixed VSCode / Multiple lines in one cell
    After finding that neither VSCode or Visual Studio correctly ran C# I did some more digging, and after a bit of modification to the .csproj file (adding LangVersion and removing ImplicitUsings) VSCode was able to identify that I had the wrong version of .NET Core installed and I was able to get my code to debug properly.

--- Capitalization.cs
+++ Capitalization.cs
@@ -7,29 +7,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World");
-    }
-}
-
-
-class Program2
-{
-    static void run(string[] args)
-    {
         Console.WriteLine("Does Capitalization Matter?");
-    }
-}
-
-class Program3
-{
-    static void run(string[] args)
-    {
         Console.WriteLine("Doesn't seem like it");
-    }
-}
-class Program4
-{
-    static void run(string[] args)
-    {
         // Capitalization matters a lot
         Console.WriteLine("I was wrong, it does matter");
     }
